134d1c01e1fd8379a59cee57850ab66d317a6392
L Dettling <tdettling@tds.net>
2024-02-06 21:39:22 -0500

    wip: broken testing
    Signed-off-by: L Dettling <tdettling@tds.net>

--- VoronoiWithThreads.jl
+++ VoronoiWithThreads.jl
@@ -3,49 +3,35 @@
 # to the Voronoi Diagram
 # This version has a larger area of 4096 x 4096
 # This version uses threads to parallelize the execution of the code
-# These are outputs produced by the execution of this program
-# on a Windows machine with an Intel microprocessor with 6 cores
-# and 12 Gigabytes of main memory:
-# Intel(R) Core(TM) i5-8400 CPU @ 2.80GHz   2.81 GHz
-# 12.0 GB (11.8 GB usable)
-# In sequential mode:
-# C:\Users\Owner\AppData\Local\Programs\Julia-1.7.2\bin\julia.exe --threads 1 VoronoiWithThreads.jl
-# 0.293455 seconds (141.52 k allocations: 8.098 MiB, 21.86% compilation time)
-# 0.232730 seconds (6 allocations: 688 bytes)
-# 1234
-# In Parallel - With 6 threads:
-# C:\Users\Owner\AppData\Local\Programs\Julia-1.7.2\bin\julia.exe --threads 6 VoronoiWithThreads.jl
-# 0.109866 seconds (141.62 k allocations: 8.105 MiB, 62.42% compilation time)
-# 0.044498 seconds (32 allocations: 3.703 KiB)
-# 1234
-# The speedup is almost 6
 
 using Base.Threads
 
 const SIZE_AREA_NT = 10
 const NSEEDS = 4
 
-global voronoi = zeros(Int32,SIZE_AREA_NT,SIZE_AREA_NT)
-global seeds = [1 1;1 SIZE_AREA_NT;SIZE_AREA_NT 1;SIZE_AREA_NT SIZE_AREA_NT]
+global voronoi = zeros(Int32, SIZE_AREA_NT, SIZE_AREA_NT)
+global seeds = [(1, 1);
+                (1, SIZE_AREA_NT);
+                (SIZE_AREA_NT, 1);
+                (SIZE_AREA_NT, SIZE_AREA_NT)]
 
 function calculateDiagram(voronoi, seeds, size)
-    
-    @threads for j in 1:size
+    #@threads
+    for j in 1:size
         for i in 1:size
             shortestDistance = typemax(Int32)
             closestSeed = 0
             for k in 1:NSEEDS
-                distance = (i - seeds[k,1])^2 + (j - seeds[k,2])^2
+                distance = (i - seeds[k][1])^2 + (j - seeds[k][2])^2
                 distance = sqrt(distance)
                 if distance < shortestDistance
                     shortestDistance = distance
                     closestSeed = k
                 end
             end
-            voronoi[i,j] = closestSeed
+            voronoi[i, j] = closestSeed
         end
     end
-    #print(voronoi)
     return voronoi
 end
 
@@ -55,20 +41,24 @@ function printGrid(grid_to_print)
     end
 end
 
-#@profile calculateDiagram(voronoi, seeds)
-#@time calculateDiagram(voronoi, seeds)
+function toPointGrid(voronoi, seeds)
+    newGrid = Tuple{Int, Int}[][]
 
-#@time calculateDiagram(voronoi, seeds)
+    for row in 1:size(voronoi, 1)
+        row_values = Tuple{Int, Int}[] 
+        for col in 1:size(voronoi, 2)
+            seedIndex = voronoi[row, col]
+            push!(row_values, seeds[seedIndex])
+        end
+        push!(newGrid, row_values)
+    end
 
-#print(voronoi)
+    return newGrid
+end
 
-#= Profile.print()
-print(voronoi[1,1])
-print(voronoi[1,SIZE_AREA_NT])
-print(voronoi[SIZE_AREA_NT,1])
-print(voronoi[SIZE_AREA_NT,SIZE_AREA_NT])
-=#
 
-#println(typeof(voronoi))
 newDiagram = calculateDiagram(voronoi, seeds, SIZE_AREA_NT)
-printGrid(newDiagram)
+grid = toPointGrid(newDiagram, seeds)
+printGrid(grid)
+print(size(grid))
+print(typeof(grid))
