bfcdc3469dc376bed85b41a6cf868c2b471716e7
L Dettling <tdettling@tds.net>
2024-02-07 21:01:31 -0500

    updating tests
    Signed-off-by: L Dettling <tdettling@tds.net>

--- tester.jl
+++ tester.jl
@@ -4,20 +4,71 @@ include("VoronoiWithThreads.jl")
 
 # Define a test function
 function test_voronoi_small1()
+    #initalization of stuff
     seeds = [Point(-3, 3), Point(3, -3), Point(-3, -3), Point(3,3)]
-    size_of_grid = 6
-    voronoi_bound = getBoundary()
+    size_of_grid = 7
     grid = Matrix{Point}(undef, size_of_grid + 1, size_of_grid + 1)
+    voronoi_bound = getBoundary(size_of_grid)
+
+    #populating the voronoi grid with actual points
+    assignGrid(grid, voronoi_bound)
     quadtree = create_tree(voronoi_bound)
 
+    #adding in seeds
     for seed in seeds
-        # seed will be closest to itself
+        #seed will be closest to itself
         insertNode(quadtree, seed, seed)
     end
 
-    # Create a 2D array to represent the voronoi diagram
-    final_diagram = generateVoronoi(quadtree, voronoi_bound, grid)
+    final_diagram = generateVoronoi(quadtree, grid, seeds)
+    # You can customize the actual and expected diagrams based on your expectations
+    expected_diagram = brute_force_partition(seeds, grid)
+    
+    @test final_diagram == expected_diagram
+end
 
+function test_voronoi_small2()
+    #initalization of stuff
+    seeds = [Point(-3, 3), Point(3, -3), Point(-3, -3), Point(3,3)]
+    size_of_grid = 11
+    grid = Matrix{Point}(undef, size_of_grid + 1, size_of_grid + 1)
+    voronoi_bound = getBoundary(size_of_grid)
+
+    #populating the voronoi grid with actual points
+    assignGrid(grid, voronoi_bound)
+    quadtree = create_tree(voronoi_bound)
+
+    #adding in seeds
+    for seed in seeds
+        #seed will be closest to itself
+        insertNode(quadtree, seed, seed)
+    end
+
+    final_diagram = generateVoronoi(quadtree, grid, seeds)
+    # You can customize the actual and expected diagrams based on your expectations
+    expected_diagram = brute_force_partition(seeds, grid)
+    
+    @test final_diagram == expected_diagram
+end
+
+function test_voronoi_small3()
+    #initalization of stuff
+    seeds = [Point(4, 7), Point(9,1), Point(2,3), Point(0,0)]
+    size_of_grid = 25
+    grid = Matrix{Point}(undef, size_of_grid + 1, size_of_grid + 1)
+    voronoi_bound = getBoundary(size_of_grid)
+
+    #populating the voronoi grid with actual points
+    assignGrid(grid, voronoi_bound)
+    quadtree = create_tree(voronoi_bound)
+
+    #adding in seeds
+    for seed in seeds
+        #seed will be closest to itself
+        insertNode(quadtree, seed, seed)
+    end
+
+    final_diagram = generateVoronoi(quadtree, grid, seeds)
     # You can customize the actual and expected diagrams based on your expectations
     expected_diagram = brute_force_partition(seeds, grid)
     
@@ -26,5 +77,8 @@ end
 
 # Run the test
 @testset "Voronoi Diagram Test" begin
+    #small batches for comple errors (not real tests)
     test_voronoi_small1()
+    test_voronoi_small2()
+    test_voronoi_small3()
 end
